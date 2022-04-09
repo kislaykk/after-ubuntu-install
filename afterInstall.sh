48e09d3fd6fb24d3bc10cd53118f7a53b25a0319
Kislay Kanti Dhar <37491602+kislaykk@users.noreply.github.com>
2022-04-09 11:08:33 +0530

    snap and apt commands to install tools

---
+++ afterInstall.sh
@@ -0,0 +1,49 @@
+
+
+# install vs code
+
+sudo snap install code --classic || true
+
+
+
+# install git
+
+sudo apt-get update || true
+
+sudo apt-get install git || true
+
+
+
+# install spotify
+
+sudo snap install spotify || true
+
+
+
+# install vlc
+
+sudo snap install vlc || true 
+
+
+
+# install brave 
+
+sudo snap install brave || true 
+
+
+
+# install g++ compiler
+
+sudo apt install g++ || true
+
+
+
+# install curl
+
+sudo apt install curl || true
+
+
+
+# install nvm
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash || true
