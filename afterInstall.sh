512b09e5198725f38bc0f6594cc6be0b8efcf45e
kislaykk <kislayof@gmail.com>
2022-04-09 20:11:16 +0530

    rvm install added

--- afterInstall.sh
+++ afterInstall.sh
@@ -47,3 +47,14 @@ sudo apt install curl || true
 # install nvm
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash || true
+
+# install rvm -> ruby version manager
+
+sudo apt-get install software-properties-common
+
+sudo apt-add-repository -y ppa:rael-gc/rvm
+sudo apt-get update
+sudo apt-get install rvm
+
+sudo usermod -a -G rvm $USER
+
